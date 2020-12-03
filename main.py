e87f02e1897d4f17ca5085fa977cda6d699c04ff
Golor <c1045132383@gmail.com>
2020-12-03 12:33:06 +0800

    修改find_latest_lecture函数 新增函数enroll

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ if __name__ == '__main__':
     login_url = 'https://lexiangla.com/login/'
     url = 'https://lexiangla.com/events'
     user_agent = "Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/87.0.4280.66Safari/537.36"
-    lecture_keyword = input('请输入讲座主题关键字：')
+    # lecture_keyword = input('请输入讲座主题关键字：')
+    lecture_keyword = "仰望星空，脚踏实地--对当代大学生国际视野培养的思考"
     CRAWLER = Crawler(user_agent=user_agent, login_url=login_url, url=url)
     CRAWLER.get_html()
     CRAWLER.click_lecture_button()
     CRAWLER.find_latest_lecture(lecture_keyword)
-    # CRAWLER.refresh(1)
+    CRAWLER.enroll()
