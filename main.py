02e21ebca6c6280064a3d2f841ddef7a4ea9e81e
Golor <c1045132383@gmail.com>
2021-04-11 20:36:06 +0800

    使用schedule完成定时功能

--- main.py
+++ main.py
@@ -9,15 +9,44 @@ Date:2020/11/27
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from selenium import webdriver
-
+import schedule
 from Crawler import Crawler
+import time
+
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    lecture_keyword = input('请输入讲座主题关键字：')
+
+def job(keyword):
     crawler = Crawler()
     crawler.get_html()
     crawler.click_lecture_button()
-    crawler.find_latest_lecture(lecture_keyword)
+    crawler.find_latest_lecture(keyword)
     crawler.enroll()
+
+
+if __name__ == '__main__':
+    lecture_keyword = input('请输入讲座主题关键字：')
+    # 测试
+    # job(keyword=lecture_keyword)
+
+    print('请选择抢讲座时间：')
+    print('1 12:00讲座')
+    print('2 17:00讲座')
+    time_mode = int(input("请输入选择："))
+    start_times = {
+        1: '11:59:20',
+        2: '16:59:20',
+    }
+    end_times = {
+        1: '12:00:30',
+        2: '17:00:30',
+    }
+    print('定时任务已开始')
+    # schedule 添加任务
+    schedule.every().day.at(start_times[time_mode]).do(lambda: job(keyword=lecture_keyword))
+    # schedule 添加退出任务
+    schedule.every().day.at(end_times[time_mode]).do(lambda: exit(0))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
